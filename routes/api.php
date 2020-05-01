f298b2bbe795147de8f544e03fa623dca100a8c6
jomasim <simjoms@gmail.com>
2020-05-01 08:12:15 +0300

    adds product controller adds product model implements get all products

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('/pizza', function (Request $request) {
-    return "Welcome to pizza api";
-});
+
+Route::get('/products', 'ProductsController@getProducts');
